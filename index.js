35bda2c2285c7809c60f4cb502e8d3401e3dbf02
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 20:43:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ function driversByName(driver){
 
 function totalRevenue(driver){
   const reduceRev = function(){
-    driver.forEach(function(d){
-      if (d === "revenue"){
-        return driver[d]
-      }
-    })
+    // driver.forEach(function(d){
+    //   if (d === "revenue"){
+    //     return driver[d]
+    //   }
+    // })
     for(const key in driver){
       if (key === "revenue"){
         return driver[key];
